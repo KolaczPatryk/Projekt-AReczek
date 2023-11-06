2bedf860c54d88c7f480c59f87d0a7cafbefce7a
m.skoncej <28263@s.pm.szczecin.pl>
2023-11-06 15:10:47 +0100

    Added Android Logcat + fixed slow build

--- HowToTry/Assets/Scripts/FileManager.cs
+++ HowToTry/Assets/Scripts/FileManager.cs
@@ -13,27 +13,22 @@ public class FileManager : MonoBehaviour
     private string selectedFilePath;
     public void OpenFile()
     {
-        
-        AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
-        AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
-        intentObject.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_GET_CONTENT"));
-        intentObject.Call<AndroidJavaObject>("setType", "application/fbx, application/obj, application/vnd.ms-3d, model/vnd.collada, application/x-3ds, application/x-blender, text/plain, application/pcl");
 
+        //AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
+        //AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
+        //intentObject.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_GET_CONTENT"));
+        //intentObject.Call<AndroidJavaObject>("setType", "application/fbx, application/obj, application/vnd.ms-3d, model/vnd.collada, application/x-3ds, application/x-blender, text/plain, application/pcl");
 
         AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
         AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
+        AndroidJavaObject intent = currentActivity.Call<AndroidJavaObject>("getIntent");
 
-        // SprawdŸ, czy u¿ytkownik ma uprawnienia do odczytu plików z pamiêci urz¹dzenia
-        bool hasPermission = Permission.HasUserAuthorizedPermission(Permission.ExternalStorageRead);
+        AndroidJavaClass intentClass = currentActivity.Call<AndroidJavaClass>("getIntent");
+        //AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
+        intent.Call<AndroidJavaObject>("setType", "application/fbx, application/obj, application/vnd.ms-3d, model/vnd.collada, application/x-3ds, application/x-blender, text/plain, application/pcl");
+        intent.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_GET_CONTENT"));
 
-        // Wyœwietl komunikat o b³êdzie, jeœli u¿ytkownik nie ma uprawnieñ
-        /*if (!hasPermission)
-        {
-            Debug.Log("U¿ytkownik nie ma uprawnieñ do odczytu plików z pamiêci urz¹dzenia.");
-            return;
-        }*/
-            currentActivity.Call("startActivityForResult", intentObject, 0);
-        
+
     } // Zmienna do przechowywania œcie¿ki wybranego pliku
 
     // Ta metoda obs³uguje rezultat wyboru pliku

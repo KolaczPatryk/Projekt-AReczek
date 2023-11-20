75fc118be380b77d75f7d867f06af374668514e6
michalfalek5 <78847213+michalfalek5@users.noreply.github.com>
2023-11-20 16:44:11 +0100

    Revert "Merge branch 'main' of https://github.com/KolaczPatryk/Projekt-AReczek"
    This reverts commit a8333594abb511a24be391393154eae8544264a8, reversing
    changes made to 9d2d998be8e312850ae1f8993c835cc89b5ec1aa.

--- HowToTry/Assets/loadURL.cs
+++ HowToTry/Assets/loadURL.cs
@@ -11,7 +11,7 @@ public class loadURL : MonoBehaviour
     void Start()
     {
         string url = holdURL.linkURL;
-        Debug.Log(url + "2137KURWO");
         echo3D.GetComponent<Echo3DHologram>().queryURL = url;
+
     }
 }
